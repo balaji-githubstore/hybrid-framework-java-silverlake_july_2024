99cdd09c901d3c67265271507725b70281511fff
Balaji <balaji0017@gmail.com>
2024-07-12 18:13:49 +0100

    Final Code

--- src/test/java/com/silverlake/base/AutomationWrapper.java
+++ src/test/java/com/silverlake/base/AutomationWrapper.java
@@ -1,11 +1,15 @@
 package com.silverlake.base;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Optional;
@@ -16,15 +20,28 @@ public class AutomationWrapper {
 	protected WebDriver driver;
 
 	@BeforeMethod
-	@Parameters({ "browser" })
-	public void setup(@Optional("ch") String browserName) {
+	@Parameters({ "browser", "node" })
+	public void setup(@Optional("ch") String browserName, @Optional("NA") String node) throws MalformedURLException {
 
-		if (browserName.equalsIgnoreCase("edge")) {
-			driver = new EdgeDriver();
-		} else if (browserName.equalsIgnoreCase("edge")) {
-			driver = new FirefoxDriver();
+		if (node.equalsIgnoreCase("NA")) {
+			if (browserName.equalsIgnoreCase("edge")) {
+				driver = new EdgeDriver();
+			} else if (browserName.equalsIgnoreCase("ff")) {
+				driver = new FirefoxDriver();
+			} else {
+				driver = new ChromeDriver();
+			}
 		} else {
-			driver = new ChromeDriver();
+			DesiredCapabilities cap = new DesiredCapabilities();
+			if (browserName.equalsIgnoreCase("edge")) {
+				cap.setBrowserName("edge");
+			} else if (browserName.equalsIgnoreCase("ff")) {
+				cap.setBrowserName("chrome");
+			} else {
+				cap.setBrowserName("chrome");
+			}
+
+			driver = new RemoteWebDriver(new URL(node), cap);
 		}
 
 		driver.manage().window().maximize();
